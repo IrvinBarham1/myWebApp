2e7b8ea6cd6f7ddc5fdb412605484d70600efef5
Irvin <irvinbarham@gmail.com>
2025-02-22 19:32:04 -0600

    new changes

--- routes/report.js
+++ routes/report.js
@@ -4,9 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/report', (req, res, next) => {
+    console.log(req.body);
     const income = parseFloat(req.body.income);
     const expenses = parseFloat(req.body.expenses);
     const savings = parseFloat(req.body.savings);
+    const name = req.body.name;
+    const month = req.body.months;
 
     const netSavings = income - expenses;
     const goalStatus = netSavings >= savings ? "Goal Met" : "Goal Not Met";
@@ -14,7 +17,9 @@ router.post('/report', (req, res, next) => {
     // Render the report.ejs file with the calculated values
     res.render(path.join(__dirname, '../', 'views', 'report.ejs'), {
         netSavings: netSavings,
-        goalStatus: goalStatus
+        goalStatus: goalStatus,
+        name : name,
+        month : month
     });
 });
 
