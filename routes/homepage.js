d196e9cb625a7319dc9327c05398102ac2b0c047
Irvin <irvinbarham@gmail.com>
2025-03-08 15:14:36 -0600

    adding new web elements

--- routes/homepage.js
+++ routes/homepage.js
@@ -5,11 +5,33 @@ const e = require('express');
 
 const router = express.Router();
 
+const getDb = require('../util/database').getDb;
+
 router.get('/', (req, res, next) => {
-    // res.sendFile(path.join(__dirname, '../', 'views', 'homepage.ejs'));
-     res.render(path.join(__dirname, '../', 'views', 'homepage.ejs'), {
-         pageTitle: 'Create Entry',
-    });
-});
+    const db = getDb();
+    let name = [];
+    db.collection('users').find({}, { projection: {name: 1}}).toArray()
+    .then(users => {
+        //pull a previous name from the database
+        for(const element of users){
+            name.push(element.name + ' ');
+        }
+        /*
+        if (name.length > 2) {
+            for(let i = 3 ; i < name.length; i++) {
+            db.collection('users').dropIndex();
+            }
+        }
+        */
+        res.render(path.join(__dirname, '../', 'views', 'homepage.ejs'), {
+            pageTitle: 'Create Entry',
+            dbName: 'Database names: ' + name
+        })
+    })
+    .catch(err => {
+        console.log("Error fetching users: ", err);
+        res.status(500).send("Internal Server Error");
+    })
+})
 
 module.exports = router;
